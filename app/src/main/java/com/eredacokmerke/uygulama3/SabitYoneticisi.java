6479039a0b8afe5471737931a58f3d93520df93f
eredacokmerke <eredacokmerke@gmail.com>
2016-09-03 05:58:28 +0300

    Sabit Yoneticisi eklendi

---
+++ app/src/main/java/com/eredacokmerke/uygulama3/SabitYoneticisi.java
@@ -0,0 +1,9 @@
+package com.eredacokmerke.uygulama3;
+
+public class SabitYoneticisi
+{
+    //xml dosya adlari
+    public static final String XML_DOSYA_UZANTISI = "xml";
+    public static final String XML_DOSYA_ADI = "new." + XML_DOSYA_UZANTISI;
+    public static final String XML_AYAR_DOSYA_ADI = "opt." + XML_DOSYA_UZANTISI;
+}
